ffa2f8ed1a579e5d6ec48d7db113904da831e107
Kazumi Inada <hello@nandenjin.com>
2022-05-18 20:48:16 +0900

    feat: Implement new theme (#25)

---
+++ .vuepress/theme/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  head: [
+    [
+      "link",
+      {
+        href: "https://fonts.googleapis.com/css2?family=Questrial&display=swap",
+        rel: "stylesheet",
+      },
+    ],
+  ],
+};
